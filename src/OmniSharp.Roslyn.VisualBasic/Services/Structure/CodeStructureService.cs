bf30f970389a297efb8ee17db29a4bfcaefadc08
Youssef Victor <youssefvictor00@gmail.com>
2021-04-04 16:52:37 +0200

    Add more services

---
+++ src/OmniSharp.Roslyn.VisualBasic/Services/Structure/CodeStructureService.cs
@@ -0,0 +1,357 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using OmniSharp.Extensions;
+using OmniSharp.Mef;
+using OmniSharp.Models.V2;
+using OmniSharp.Models.V2.CodeStructure;
+using OmniSharp.Roslyn.Utilities;
+using OmniSharp.Services;
+
+namespace OmniSharp.Roslyn.VisualBasic.Services.Structure
+{
+    [OmniSharpHandler(OmniSharpEndpoints.V2.CodeStructure, LanguageNames.VisualBasic)]
+    public class CodeStructureService : IRequestHandler<CodeStructureRequest, CodeStructureResponse>
+    {
+        private readonly OmniSharpWorkspace _workspace;
+        private readonly IEnumerable<ICodeElementPropertyProvider> _propertyProviders;
+
+        [ImportingConstructor]
+        public CodeStructureService(
+            OmniSharpWorkspace workspace,
+            [ImportMany] IEnumerable<ICodeElementPropertyProvider> propertyProviders)
+        {
+            _workspace = workspace;
+            _propertyProviders = propertyProviders;
+        }
+
+        public async Task<CodeStructureResponse> Handle(CodeStructureRequest request)
+        {
+            // To provide complete code structure for the document wait until all projects are loaded.
+            var document = await _workspace.GetDocumentFromFullProjectModelAsync(request.FileName);
+            if (document == null)
+            {
+                return null;
+            }
+
+            var elements = await GetCodeElementsAsync(document);
+
+            var response = new CodeStructureResponse
+            {
+                Elements = elements
+            };
+
+            return response;
+        }
+
+        private async Task<IReadOnlyList<CodeElement>> GetCodeElementsAsync(Document document)
+        {
+            var text = await document.GetTextAsync();
+            var syntaxRoot = await document.GetSyntaxRootAsync();
+            var semanticModel = await document.GetSemanticModelAsync();
+
+            var results = ImmutableList.CreateBuilder<CodeElement>();
+
+            foreach (var node in ((CompilationUnitSyntax)syntaxRoot).Members)
+            {
+                foreach (var element in CreateCodeElements(node, text, semanticModel))
+                {
+                    if (element != null)
+                    {
+                        results.Add(element);
+                    }
+                }
+            }
+
+            return results.ToImmutable();
+        }
+
+        private IEnumerable<CodeElement> CreateCodeElements(SyntaxNode node, SourceText text, SemanticModel semanticModel)
+        {
+            switch (node)
+            {
+                case TypeBlockSyntax typeDeclaration:
+                    yield return CreateCodeElement(typeDeclaration, text, semanticModel);
+                    break;
+                case DelegateStatementSyntax delegateDeclaration:
+                    yield return CreateCodeElement(delegateDeclaration, text, semanticModel);
+                    break;
+                case EnumBlockSyntax enumDeclaration:
+                    yield return CreateCodeElement(enumDeclaration, text, semanticModel);
+                    break;
+                case NamespaceBlockSyntax namespaceDeclaration:
+                    yield return CreateCodeElement(namespaceDeclaration, text, semanticModel);
+                    break;
+                case MethodBlockBaseSyntax baseMethodDeclaration:
+                    yield return CreateCodeElement(baseMethodDeclaration, text, semanticModel);
+                    break;
+                case PropertyBlockSyntax propertyDeclaration:
+                    yield return CreateCodeElement(propertyDeclaration, text, semanticModel);
+                    break;
+                case FieldDeclarationSyntax fieldDeclaration:
+                    foreach (var variableDeclarator in fieldDeclaration.Declarators)
+                    {
+                        foreach (var name in variableDeclarator.Names)
+                        {
+                            yield return CreateCodeElement(name, fieldDeclaration, text, semanticModel);
+                        }
+                    }
+
+                    break;
+                case EnumMemberDeclarationSyntax enumMemberDeclarationSyntax:
+                    yield return CreateCodeElement(enumMemberDeclarationSyntax, text, semanticModel);
+                    break;
+            }
+        }
+
+        private CodeElement CreateCodeElement(TypeBlockSyntax typeDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(typeDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortTypeFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.TypeFormat)
+            };
+
+            AddRanges(builder, typeDeclaration.BlockStatement.AttributeLists.Span, typeDeclaration.Span, typeDeclaration.BlockStatement.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            foreach (var member in typeDeclaration.Members)
+            {
+                foreach (var childElement in CreateCodeElements(member, text, semanticModel))
+                {
+                    builder.AddChild(childElement);
+                }
+            }
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(DelegateStatementSyntax delegateDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(delegateDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortTypeFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.TypeFormat),
+            };
+
+            AddRanges(builder, delegateDeclaration.AttributeLists.Span, delegateDeclaration.Span, delegateDeclaration.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(EnumBlockSyntax enumDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(enumDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortTypeFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.TypeFormat),
+            };
+
+            AddRanges(builder, enumDeclaration.EnumStatement.AttributeLists.Span, enumDeclaration.Span, enumDeclaration.EnumStatement.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            foreach (var member in enumDeclaration.Members)
+            {
+                foreach (var childElement in CreateCodeElements(member, text, semanticModel))
+                {
+                    builder.AddChild(childElement);
+                }
+            }
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(NamespaceBlockSyntax namespaceDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(namespaceDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortTypeFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.TypeFormat),
+            };
+
+            AddRanges(builder, attributesSpan: default, namespaceDeclaration.Span, namespaceDeclaration.NamespaceStatement.Name.Span, text);
+
+            foreach (var member in namespaceDeclaration.Members)
+            {
+                foreach (var childElement in CreateCodeElements(member, text, semanticModel))
+                {
+                    builder.AddChild(childElement);
+                }
+            }
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(MethodBlockBaseSyntax baseMethodDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(baseMethodDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortMemberFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.MemberFormat),
+            };
+
+            AddRanges(builder, baseMethodDeclaration.BlockStatement.AttributeLists.Span, baseMethodDeclaration.Span, GetNameSpan(baseMethodDeclaration), text);
+            AddSymbolProperties(symbol, builder);
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(PropertyBlockSyntax propertyDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(propertyDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortMemberFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.MemberFormat),
+            };
+
+            AddRanges(builder, propertyDeclaration.PropertyStatement.AttributeLists.Span, propertyDeclaration.Span, propertyDeclaration.PropertyStatement.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(ModifiedIdentifierSyntax modifiedIdentifier, FieldDeclarationSyntax fieldDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(modifiedIdentifier);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortMemberFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.MemberFormat),
+            };
+
+            AddRanges(builder, fieldDeclaration.AttributeLists.Span, modifiedIdentifier.Parent.Span, modifiedIdentifier.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            return builder.ToCodeElement();
+        }
+
+        private CodeElement CreateCodeElement(EnumMemberDeclarationSyntax enumMemberDeclaration, SourceText text, SemanticModel semanticModel)
+        {
+            var symbol = semanticModel.GetDeclaredSymbol(enumMemberDeclaration);
+            if (symbol == null)
+            {
+                return null;
+            }
+
+            var builder = new CodeElement.Builder
+            {
+                Kind = symbol.GetKindString(),
+                Name = symbol.ToDisplayString(SymbolDisplayFormats.ShortMemberFormat),
+                DisplayName = symbol.ToDisplayString(SymbolDisplayFormats.MemberFormat),
+            };
+
+            AddRanges(builder, enumMemberDeclaration.AttributeLists.Span, enumMemberDeclaration.Span, enumMemberDeclaration.Identifier.Span, text);
+            AddSymbolProperties(symbol, builder);
+
+            return builder.ToCodeElement();
+        }
+
+        private static TextSpan GetNameSpan(MethodBlockBaseSyntax baseMethodDeclaration)
+        {
+            switch (baseMethodDeclaration)
+            {
+                case AccessorBlockSyntax accessorBlock:
+                    return accessorBlock.AccessorStatement.AccessorKeyword.Span;
+                case ConstructorBlockSyntax constructorBlock:
+                    return constructorBlock.SubNewStatement.NewKeyword.Span;
+                case MethodBlockSyntax methodBlock:
+                    return methodBlock.SubOrFunctionStatement.Identifier.Span;
+                case OperatorBlockSyntax operatorBlock:
+                    return operatorBlock.OperatorStatement.OperatorToken.Span;
+                default:
+                    return default;
+            }
+        }
+
+        private static void AddRanges(CodeElement.Builder builder, TextSpan attributesSpan, TextSpan fullSpan, TextSpan nameSpan, SourceText text)
+        {
+            if (attributesSpan != default)
+            {
+                builder.AddRange(SymbolRangeNames.Attributes, text.GetRangeFromSpan(attributesSpan));
+            }
+
+            if (fullSpan != default)
+            {
+                builder.AddRange(SymbolRangeNames.Full, text.GetRangeFromSpan(fullSpan));
+            }
+
+            if (nameSpan != default)
+            {
+                builder.AddRange(SymbolRangeNames.Name, text.GetRangeFromSpan(nameSpan));
+            }
+        }
+
+        private void AddSymbolProperties(ISymbol symbol, CodeElement.Builder builder)
+        {
+            var accessibility = symbol.GetAccessibilityString();
+            if (accessibility != null)
+            {
+                builder.AddProperty(SymbolPropertyNames.Accessibility, accessibility);
+            }
+
+            builder.AddProperty(SymbolPropertyNames.Static, symbol.IsStatic);
+
+            foreach (var propertyProvider in _propertyProviders)
+            {
+                foreach (var (name, value) in propertyProvider.ProvideProperties(symbol))
+                {
+                    builder.AddProperty(name, value);
+                }
+            }
+        }
+    }
+}
---
+++ src/OmniSharp.Roslyn.VisualBasic/Workers/StructureComputer.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using OmniSharp.Abstractions.Services;
+using OmniSharp.Models;
+using OmniSharp.Models.MembersTree;
+
+namespace OmniSharp
+{
+    public class StructureComputer : VisualBasicSyntaxWalker
+    {
+        private readonly Stack<FileMemberElement> _roots = new Stack<FileMemberElement>();
+        private readonly IEnumerable<ISyntaxFeaturesDiscover> _featureDiscovers;
+        private string _currentProject;
+        private SemanticModel _semanticModel;
+
+        public static async Task<IEnumerable<FileMemberElement>> Compute(
+            IEnumerable<Document> documents,
+            IEnumerable<ISyntaxFeaturesDiscover> featureDiscovers)
+        {
+            var root = new FileMemberElement() { ChildNodes = new List<FileMemberElement>() };
+            var visitor = new StructureComputer(root, featureDiscovers);
+
+            foreach (var document in documents)
+            {
+                await visitor.Process(document);
+            }
+
+            return root.ChildNodes;
+        }
+
+        public static Task<IEnumerable<FileMemberElement>> Compute(IEnumerable<Document> documents) 
+            => Compute(documents, Enumerable.Empty<ISyntaxFeaturesDiscover>());
+
+        private StructureComputer(FileMemberElement root, IEnumerable<ISyntaxFeaturesDiscover> featureDiscovers)
+        {
+            _featureDiscovers = featureDiscovers ?? Enumerable.Empty<ISyntaxFeaturesDiscover>();
+            _roots.Push(root);
+        }
+
+        private async Task Process(Document document)
+        {
+            _currentProject = document.Project.Name;
+
+            if (_featureDiscovers.Any(dis => dis.NeedSemanticModel))
+            {
+                _semanticModel = await document.GetSemanticModelAsync();
+            }
+
+            var syntaxRoot = await document.GetSyntaxRootAsync();
+            (syntaxRoot as VisualBasicSyntaxNode)?.Accept(this);
+        }
+
+        private FileMemberElement AsNode(SyntaxNode node, string text, Location location, TextSpan attributeSpan)
+        {
+            var ret = new FileMemberElement();
+            var lineSpan = location.GetLineSpan();
+            ret.Projects = new List<string>();
+            ret.ChildNodes = new List<FileMemberElement>();
+            ret.Kind = node.Kind().ToString();
+            ret.AttributeSpanStart = attributeSpan.Start;
+            ret.AttributeSpanEnd = attributeSpan.End;
+            ret.Location = new QuickFix()
+            {
+                Text = text,
+                FileName = lineSpan.Path,
+                Line = lineSpan.StartLinePosition.Line,
+                Column = lineSpan.StartLinePosition.Character,
+                EndLine = lineSpan.EndLinePosition.Line,
+                EndColumn = lineSpan.EndLinePosition.Character
+            };
+
+            foreach (var featureDiscover in _featureDiscovers)
+            {
+                var features = featureDiscover.Discover(node, _semanticModel);
+                foreach (var feature in features)
+                {
+                    ret.Features.Add(feature);
+                }
+            }
+
+            return ret;
+        }
+
+        private FileMemberElement AsChild(SyntaxNode node, string text, Location location, TextSpan attributeSpan)
+        {
+            var child = AsNode(node, text, location, attributeSpan);
+            var childNodes = ((List<FileMemberElement>)_roots.Peek().ChildNodes);
+
+            // Prevent inserting the same node multiple times
+            // but make sure to insert them at the right spot
+            var idx = childNodes.BinarySearch(child);
+            if (idx < 0)
+            {
+                ((List<string>)child.Projects).Add(_currentProject);
+                childNodes.Insert(~idx, child);
+                return child;
+            }
+            else
+            {
+                ((List<string>)childNodes[idx].Projects).Add(_currentProject);
+                return childNodes[idx];
+            }
+        }
+
+        private FileMemberElement AsParent(SyntaxNode node, string text, Action fn, Location location, TextSpan attributeSpan)
+        {
+            var child = AsChild(node, text, location, attributeSpan);
+            _roots.Push(child);
+            fn();
+            _roots.Pop();
+            return child;
+        }
+
+        public override void VisitClassBlock(ClassBlockSyntax node)
+        {
+            AsParent(node, node.ClassStatement.Identifier.Text, () => base.VisitClassBlock(node), node.ClassStatement.Identifier.GetLocation(), node.ClassStatement.AttributeLists.Span);
+        }
+
+        public override void VisitInterfaceBlock(InterfaceBlockSyntax node)
+        {
+            AsParent(node, node.InterfaceStatement.Identifier.Text, () => base.VisitInterfaceBlock(node), node.InterfaceStatement.Identifier.GetLocation(), node.BlockStatement.AttributeLists.Span);
+        }
+
+        public override void VisitEnumBlock(EnumBlockSyntax node)
+        {
+            AsParent(node, node.EnumStatement.Identifier.Text, () => base.VisitEnumBlock(node), node.EnumStatement.Identifier.GetLocation(), node.EnumStatement.AttributeLists.Span);
+        }
+
+        public override void VisitEnumMemberDeclaration(EnumMemberDeclarationSyntax node)
+        {
+            AsChild(node, node.Identifier.Text, node.Identifier.GetLocation(), node.AttributeLists.Span);
+        }
+
+        public override void VisitPropertyBlock(PropertyBlockSyntax node)
+        {
+            AsChild(node, node.PropertyStatement.Identifier.Text, node.PropertyStatement.Identifier.GetLocation(), node.PropertyStatement.AttributeLists.Span);
+        }
+
+        public override void VisitMethodBlock(MethodBlockSyntax node)
+        {
+            AsChild(node, node.SubOrFunctionStatement.Identifier.Text, node.SubOrFunctionStatement.Identifier.GetLocation(), node.SubOrFunctionStatement.AttributeLists.Span);
+        }
+
+        public override void VisitFieldDeclaration(FieldDeclarationSyntax node)
+        {
+            AsChild(node, node.Declarators.First().Names.First().Identifier.Text, node.Declarators.First().Names.First().Identifier.GetLocation(), node.AttributeLists.Span);
+        }
+
+        public override void VisitEventBlock(EventBlockSyntax node)
+        {
+            AsChild(node, node.EventStatement.Identifier.Text, node.EventStatement.Identifier.GetLocation(), node.EventStatement.AttributeLists.Span);
+        }
+
+        public override void VisitStructureBlock(StructureBlockSyntax node)
+        {
+            AsParent(node, node.StructureStatement.Identifier.Text, () => base.VisitStructureBlock(node), node.StructureStatement.Identifier.GetLocation(), node.StructureStatement.AttributeLists.Span);
+        }
+
+        public override void VisitConstructorBlock(ConstructorBlockSyntax node)
+        {
+            AsChild(node, node.SubNewStatement.NewKeyword.Text, node.SubNewStatement.NewKeyword.GetLocation(), node.SubNewStatement.AttributeLists.Span);
+        }
+    }
+}
